b8c796256f2c67a1cba58fc1f4c4e9a31519cf8b
Zach <zachsneeringerdev@gmail.com>
2022-07-29 02:31:13 -0400

    client section functional + fixups

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -3,13 +3,13 @@
 
 const mobileMenu = () => {
 
-    // variables
+    // Variables
     const burgerIcon = document.querySelector('.burgerIcon');
     const mobileMenu = document.querySelector('.mobileMenu');
     const links = [...document.querySelectorAll('.mobileMenu > *')];
     let menuShowing = false;
 
-    // functions
+    // Functions
     const handleMenu = () => {
         burgerIcon.classList.toggle('clicked');
         if (!menuShowing) {
@@ -35,14 +35,55 @@ const mobileMenu = () => {
         }
     }
 
-    // events
+    // Events
     burgerIcon.addEventListener('click', handleMenu);
 
 }
 
+const typeWriter = () => {
+
+    // Variables
+    let COUNTER = 1;
+    const arrayUls = [...document.querySelectorAll('aside > ul')];
+
+    // Functions
+    const logic = () => {
+        // 3 UL's total, can't count too far
+        if (COUNTER === 3) COUNTER = 0;
+        let ULindex = COUNTER++;
+        // display the next UL element
+        classChange(ULindex);
+        let clientNames = [...arrayUls[ULindex].children];
+        // send each individual client to helper function 
+        clientNames.forEach(client => TYPE(client));
+    }
+
+    // run loop through client to display text
+    const TYPE = (client) => {
+        const CLIENTtext = client.textContent;
+        client.textContent = '';
+        for (let z = 0; z < CLIENTtext.length; z++) {
+            setTimeout(() => {
+                client.textContent += CLIENTtext[z];
+            }, 60 * z);
+        }
+    }
+
+    const classChange = (ULindex) => {
+        arrayUls.forEach(UL => UL.classList.remove('helloClients'));
+        arrayUls[ULindex].classList.add('helloClients');
+    }
+
+    // Interval
+    setInterval(logic, 6000);
+    
+}
+
+
 
 const riorku = () => {
     mobileMenu();
+    typeWriter();
 }
 
 riorku();

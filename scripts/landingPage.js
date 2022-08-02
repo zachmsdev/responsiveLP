c3b50e345101abe12bd30dc7a465a5bec291043f
Zach <zachsneeringerdev@gmail.com>
2022-08-02 00:07:12 -0400

    HTML, JS finished, safari bug only thing left

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -40,6 +40,7 @@ const mobileMenu = () => {
 
 }
 
+
 const typeWriter = () => {
 
     // Variables
@@ -80,10 +81,90 @@ const typeWriter = () => {
 }
 
 
+const testimonials = () => {
+
+    // Variables
+    const controls = [...document.querySelectorAll('.controls > button')];
+    const transitionalDiv = document.querySelector('.transitionDiv');
+    let clientIndex = 0;
+    let autoPlayIndex = 0;
+    const clientElements = {
+        quote: document.querySelector('blockquote p'),
+        name: document.querySelector('.clientName'),
+        city: document.querySelector('.clientCity'),
+        picture: document.querySelector('.profileBall'),
+        forwardButton: document.querySelector('.forward'),
+        backButton: document.querySelector('.previous')
+    };
+    const clientContent = [
+        {
+            name: 'Jared Locks',
+            location: 'Baltimore, MD',
+            photo: '../images/client1.jpg',
+            quote: "\"At Riorku they have the best team of interior designers, architects, engineers and manufactures to make every dream become a reality.\""
+        }, {
+            name: 'Savannah Smith',
+            location: 'Miami, FL',
+            photo: '../images/client2.jpg',
+            quote: "\"Riorku's architects, engineers and manufactures helped to design my dream home and made it exactly how I dreamed that it should look!\""
+        }, {
+            name: 'Dennis Bates',
+            location: 'Denver, CO',
+            photo: '../images/client3.jpg',
+            quote: "\"Wow, I can't believe how good the architects and designers are at Riorku. Now my home exactly matches with my own personal style!\""
+        }
+    ];
+    
+    // Functions
+    const handleControls = (e) => {
+        if (e.target.classList.contains('forward')) forwardPress();
+        if (e.target.classList.contains('previous')) backPress();
+    }
+
+    const forwardPress = () => {
+        clientElements.forwardButton.classList.remove('focusAnimation');
+        if (clientIndex === 1) clientElements.forwardButton.disabled = true;
+        if (clientElements.backButton.disabled === true) clientElements.backButton.disabled = false;
+        if (clientIndex < 2) {
+            clientIndex++;
+            updateResults();
+        } 
+    }
+
+    const backPress = () => {
+        if (clientElements.forwardButton.disabled === true) clientElements.forwardButton.disabled = false;
+        if (clientIndex === 1) clientElements.backButton.disabled = true;
+        if (clientIndex > 0) {
+            clientIndex--;
+            updateResults();
+        } 
+    }
+
+    const updateResults = () => {            
+        showAnimation();
+        clientElements.quote.textContent = clientContent[clientIndex].quote;
+        clientElements.picture.style.backgroundImage = `url(${clientContent[clientIndex].photo})`;
+        clientElements.name.textContent = clientContent[clientIndex].name;
+        clientElements.city.textContent = clientContent[clientIndex].location;
+    }
+
+    const showAnimation = () => {
+        transitionalDiv.classList.add('divAnimation');
+        transitionalDiv.onanimationend = () => {
+            transitionalDiv.classList.remove('divAnimation');
+        }
+    }
+
+    // Events
+    controls.forEach(control => control.addEventListener('click', handleControls));
+
+}
+
 
 const riorku = () => {
     mobileMenu();
     typeWriter();
+    testimonials();
 }
 
 riorku();
